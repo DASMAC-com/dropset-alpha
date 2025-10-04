6678152df15a3407f8af82c2596f005d0e9e9dda
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-10-04 11:56:56 -0700

    Fix register market name

--- program/src/instructions/mod.rs
+++ program/src/instructions/mod.rs
@@ -1,9 +1,9 @@
 pub mod deposit;
 pub mod flush_events;
-pub mod register;
+pub mod register_market;
 pub mod withdraw;
 
 pub use {
     deposit::process_deposit, flush_events::process_flush_events,
-    register::process_register_market, withdraw::process_withdraw,
+    register_market::process_register_market, withdraw::process_withdraw,
 };
