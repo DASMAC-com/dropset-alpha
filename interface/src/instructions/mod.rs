74eea0b57009e35b57d72971414689aa14c985f9
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-10-30 20:02:26 -0700

    Refactor `dropset` to use the instruction macro helpers

--- interface/src/instructions/mod.rs
+++ interface/src/instructions/mod.rs
@@ -1,4 +1,4 @@
-use pinocchio::program_error::ProgramError;
+use instruction_macros::ProgramInstruction;
 
 use crate::error::DropsetError;
 
@@ -9,26 +9,24 @@ pub mod register_market;
 pub mod withdraw;
 
 #[repr(u8)]
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, ProgramInstruction)]
+#[program_id(crate::program::ID)]
 #[cfg_attr(test, derive(strum_macros::FromRepr, strum_macros::EnumIter))]
+#[rustfmt::skip]
 pub enum InstructionTag {
-    RegisterMarket,
-    Deposit,
-    Withdraw,
     CloseSeat,
+    Deposit,
+    RegisterMarket,
+    Withdraw,
     FlushEvents,
 }
 
 impl TryFrom<u8> for InstructionTag {
-    type Error = ProgramError;
+    type Error = DropsetError;
 
-    fn try_from(value: u8) -> Result<Self, Self::Error> {
-        match value {
-            // SAFETY: A valid enum variant is guaranteed with the match pattern.
-            // All variants are checked in the exhaustive instruction tag test.
-            0..5 => Ok(unsafe { core::mem::transmute::<u8, Self>(value) }),
-            _ => Err(DropsetError::InvalidInstructionTag.into()),
-        }
+    #[inline(always)]
+    fn try_from(tag: u8) -> Result<Self, Self::Error> {
+        InstructionTag_try_from_tag!(tag, DropsetError::InvalidInstructionTag)
     }
 }
 
