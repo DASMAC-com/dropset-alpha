f3ef73fb542ab614fb15cf557a4165e37bef1699
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-12-05 17:24:28 -0800

    Remove unnecessary qualification on bias in UNBIASED_* consts

--- price/src/lib.rs
+++ price/src/lib.rs
@@ -33,11 +33,11 @@ pub const BIAS: u8 = 16;
 
 /// The minimum unbiased exponent value.
 #[cfg(test)]
-const UNBIASED_MIN: i16 = 0 - crate::BIAS as i16;
+const UNBIASED_MIN: i16 = 0 - BIAS as i16;
 
 /// The maximum unbiased exponent value.
 #[cfg(test)]
-const UNBIASED_MAX: i16 = (crate::BIAS as i16) - 1;
+const UNBIASED_MAX: i16 = (BIAS as i16) - 1;
 
 /// The bitmask for the price mantissa calculated from the number of bits it uses.
 pub const PRICE_MANTISSA_MASK: u32 = u32::MAX >> ((U32_BITS - PRICE_MANTISSA_BITS) as usize);
