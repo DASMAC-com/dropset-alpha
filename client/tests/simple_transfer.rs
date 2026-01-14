264c6a7fe182f69ff9d35040be8954ab7a893a16
Matt <90358481+xbtmatt@users.noreply.github.com>
2026-01-14 13:41:33 -0800

    `INITIAL_LAMPORTS` => `LAMPORTS`

--- client/tests/simple_transfer.rs
+++ client/tests/simple_transfer.rs
@@ -6,18 +6,18 @@ use solana_system_interface::instruction::transfer;
 
 #[test]
 fn simple_transfer() {
-    const INITIAL_LAMPORTS: u64 = 10_000_000;
+    const LAMPORTS: u64 = 10_000_000;
     let alice_address = Address::new_unique();
-    let alice_account = Account::new(INITIAL_LAMPORTS, 0, &SYSTEM_PROGRAM_ID);
+    let alice_account = Account::new(LAMPORTS, 0, &SYSTEM_PROGRAM_ID);
     let bob_address = Address::new_unique();
-    let bob_account = Account::new(INITIAL_LAMPORTS, 0, &SYSTEM_PROGRAM_ID);
+    let bob_account = Account::new(LAMPORTS, 0, &SYSTEM_PROGRAM_ID);
     let mollusk = new_dropset_mollusk_context(vec![
         (alice_address, alice_account.clone()),
         (bob_address, bob_account),
     ]);
 
     // Transfer half of alice's lamports to bob.
-    let send_to_bob = transfer(&alice_address, &bob_address, INITIAL_LAMPORTS / 2);
+    let send_to_bob = transfer(&alice_address, &bob_address, LAMPORTS / 2);
 
     let get_account = |address: &Address| mollusk.account_store.borrow().get(address).cloned();
 
@@ -25,8 +25,8 @@ fn simple_transfer() {
     let bob_before = get_account(&bob_address);
     assert!(alice_before.is_some());
     assert!(bob_before.is_some());
-    assert_eq!(alice_before.unwrap().lamports, INITIAL_LAMPORTS);
-    assert_eq!(bob_before.unwrap().lamports, INITIAL_LAMPORTS);
+    assert_eq!(alice_before.unwrap().lamports, LAMPORTS);
+    assert_eq!(bob_before.unwrap().lamports, LAMPORTS);
 
     // Process the transfer instruction.
     assert!(mollusk
@@ -36,6 +36,6 @@ fn simple_transfer() {
 
     let alice_after = get_account(&alice_address).unwrap();
     let bob_after = get_account(&bob_address).unwrap();
-    assert_eq!(alice_after.lamports, INITIAL_LAMPORTS / 2);
-    assert_eq!(bob_after.lamports, INITIAL_LAMPORTS + INITIAL_LAMPORTS / 2);
+    assert_eq!(alice_after.lamports, LAMPORTS / 2);
+    assert_eq!(bob_after.lamports, LAMPORTS + LAMPORTS / 2);
 }
