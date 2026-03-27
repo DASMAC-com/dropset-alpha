700c032e4287f97502482428962fcb5a0976dbe8
Matt <90358481+xbtmatt@users.noreply.github.com>
2026-03-27 16:04:48 -0700

    Fix method name

--- services/maker-bot/src/tasks/throttled_order_update.rs
+++ services/maker-bot/src/tasks/throttled_order_update.rs
@@ -123,12 +123,12 @@ async fn handle_faucet_request(
 
     let quote_after = quote_request
         .parsed_transaction
-        .try_into_mapped_balances()?
+        .parsed_balances()?
         .get_ata_post_token_balance(&quote_ata)
         .expect("Quote balance should be mapped");
     let base_after = base_request
         .parsed_transaction
-        .try_into_mapped_balances()?
+        .parsed_balances()?
         .get_ata_post_token_balance(&base_ata)
         .expect("Base balance should be mapped");
 
