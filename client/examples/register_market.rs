c9e0757999128575e89a772cc8100d0946d2cc89
Matt <90358481+xbtmatt@users.noreply.github.com>
2026-03-27 20:10:02 -0700

    Have the register market example print verbose logs by default

--- client/examples/register_market.rs
+++ client/examples/register_market.rs
@@ -1,13 +1,29 @@
-use client::e2e_helpers::{
-    E2e,
-    User,
+use std::collections::HashSet;
+
+use client::{
+    e2e_helpers::{
+        E2e,
+        User,
+    },
+    transactions::{
+        CustomRpcClient,
+        SendTransactionConfig,
+    },
 };
 use solana_sdk::signature::Keypair;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let user = Keypair::new();
-    let e2e = E2e::new_users_and_market(None, [User::new(&user, 0, 0)]).await?;
+    let rpc = CustomRpcClient::new(
+        None,
+        Some(SendTransactionConfig {
+            compute_budget: Some(2000000),
+            debug_logs: Some(true),
+            program_id_filter: HashSet::from([dropset_interface::program::ID]),
+        }),
+    );
+    let e2e = E2e::new_users_and_market(Some(rpc), [User::new(&user, 0, 0)]).await?;
 
     println!(
         "Transaction signature: {}",
