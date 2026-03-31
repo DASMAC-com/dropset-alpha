8a383e866aa086715de80105fe7b3643ece78136
Matt <90358481+xbtmatt@users.noreply.github.com>
2026-03-31 11:27:35 -0700

    WIP

---
+++ ts-sdk/tsup.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from "tsup";
+import type { Options, Format } from "tsup";
+
+// Ensure that these option fields are not undefined.
+type MandatoryOptions = Options & {
+  outDir: string;
+  format: Format | Format[];
+};
+
+// Default config, used as a base template
+const DEFAULT_CONFIG: Options = {
+  bundle: true,
+  clean: true,
+  dts: true,
+  minify: true,
+  entry: ["src/index.ts"], // Include everything exported from src/index.ts.
+  skipNodeModulesBundle: true,
+  sourcemap: true,
+  splitting: true,
+  target: "esnext",
+  platform: "node",
+};
+
+// Common.js config
+const COMMON_CONFIG: MandatoryOptions = {
+  ...DEFAULT_CONFIG,
+  entry: ["src/**/*.ts"],
+  format: "cjs",
+  outDir: "dist/common",
+};
+
+// ESM config
+const ESM_CONFIG: MandatoryOptions = {
+  ...DEFAULT_CONFIG,
+  entry: ["src/**/*.ts"],
+  format: "esm",
+  outDir: "dist/esm",
+};
+
+export default defineConfig([COMMON_CONFIG, ESM_CONFIG]);
