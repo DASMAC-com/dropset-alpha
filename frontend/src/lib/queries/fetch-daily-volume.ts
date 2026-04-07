bea2626edaf692eb68a47e9fca68952f74177080
Matt <90358481+xbtmatt@users.noreply.github.com>
2026-04-06 18:17:21 -0700

    Only return random volume values in development

--- frontend/src/lib/queries/fetch-daily-volume.ts
+++ frontend/src/lib/queries/fetch-daily-volume.ts
@@ -6,6 +6,11 @@ import type { Address } from "@solana/addresses";
 export async function fetchDailyVolume(
   _marketAddress: Address,
 ): Promise<bigint | undefined> {
-  // Stub for now until there's a clear way to get the daily volume.
-  return BigInt(Math.trunc(Math.random() * 100_000_000));
+  if (process.env.NODE_ENV === "development") {
+    // Return a non-deterministic, randomly generated value in a development environment.
+    return BigInt(Math.trunc(Math.random() * 100_000_000));
+  } else {
+    // Stub deterministically in a non-dev environment to avoid caching random values on real infra.
+    return 0n;
+  }
 }
